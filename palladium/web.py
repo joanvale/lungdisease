6c6b41adf8a23612a41c74bf3f1e035830f31e70
joanvale <78213096+joanvale@users.noreply.github.com>
2023-05-07 11:40:34 +0800

    Update web.py

--- palladium/web.py
+++ palladium/web.py
@@ -59,14 +59,14 @@ def upload_xray_image():
 			report="Prediction:", "Looks like you are having TUBERCULOSIS."
 			symptoms="Symptoms:", "Some of the symptoms of tuberculosis are fever, tiredness, chills, weight loss, night sweats"
 		elif report.lower()=="mild pneumonia":
-			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "MILD PNEUMONIA"
-			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough."           
-	       elif report.lower()=="moderate pneumonia":
+			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "MILD PNEUMONIA" 
+			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough."
+	        elif report.lower()=="moderate pneumonia":
 			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "MODERATE PNEUMONIA"
-			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of b
-	       elif report.lower()=="severe pneumonia":
+			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough."
+	        elif report.lower()=="severe pneumonia":
 			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "SEVERE PNEUMONIA"
-			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of b
+			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough."
 	except:
 		report="Unable to process image"
 		symptoms=""
