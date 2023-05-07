353d8a0de7c6f7d13196c6b7c20a36d2302b1e17
joanvale <78213096+joanvale@users.noreply.github.com>
2023-05-07 09:00:21 +0800

    Update web.py

--- palladium/web.py
+++ palladium/web.py
@@ -58,9 +58,15 @@ def upload_xray_image():
 		elif report.lower()=="tuberculosis":
 			report="Prediction:", "Looks like you are having TUBERCULOSIS."
 			symptoms="Symptoms:", "Some of the symptoms of tuberculosis are fever, tiredness, chills, weight loss, night sweats"
-		elif report.lower()=="pneumonia":
-			report="Prediction:", "Looks like you are infected by PNEUMONIA"
+		elif report.lower()=="mild pneumonia":
+			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "MILD PNEUMONIA
 			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough."           
+	       elif report.lower()=="moderate pneumonia":
+			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "MODERATE PNEUMONIA
+			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of b
+	      elif report.lower()=="severe pneumonia":
+			report="Prediction:", "Looks like you are infected by PNEUMONIA", "Severity:", "SEVERE PNEUMONIA
+			symptoms="Symptoms:", "Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of b
 	except:
 		report="Unable to process image"
 		symptoms=""
